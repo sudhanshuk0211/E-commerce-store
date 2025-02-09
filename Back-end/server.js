03e9e0885905e062de2d5e4fa5b9f39640acbd19
Sudhanshu <sudhanshu.21b0101017@abes.ac.in>
2025-02-09 14:41:59 +0530

    user complete

--- Back-end/server.js
+++ Back-end/server.js
@@ -3,8 +3,9 @@ import dotenv from "dotenv"
 
 // routes 
 import authRoutes from "./routes/auth.route.js"
+import productRoutes from "./routes/product.route.js"
 import { connectDb } from "./lib/db.js"
-
+import cookieParser from "cookie-parser"
 dotenv.config()
 
 const app = express()
@@ -12,7 +13,10 @@ const PORT = process.env.PORT ;
 
 
 app.use(express.json()) // allows to parse the body of the request
+app.use(cookieParser())
+
 app.use("/api/auth", authRoutes)
+app.use("/api/products", productRoutes)
 
 app.listen(PORT,()=> {
     console.log("Server is running on http://localhost:" + PORT )
